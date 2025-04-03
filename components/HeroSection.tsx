2f44e1e35aaf6978e75d54671704709f9ccee56d
nathnaelteshome10 <nathnaelteshome10@gmail.com>
2025-04-03 21:52:38 +0300

    feat: init project

---
+++ components/HeroSection.tsx
@@ -0,0 +1,7 @@
+import { Hero } from "@/data/data";
+
+const HeroSection = () => {
+  return <div className="font-mono">{Hero.hTag}</div>;
+};
+
+export default HeroSection;
---
+++ data/data.ts
@@ -0,0 +1,100 @@
+export const Hero = {
+  pTag: "Hi, My name is",
+  hTag: "Nathnael Teshome",
+  hTag2: "I am a Full-Stack Developer",
+  pTag2:
+    "I am a software engineer with a passion for building web applications. I have experience in JavaScript, React, and Node.js.I am currently working at a startup where I am responsible for building and maintaining the company's web applications. I am also involved in the design and development of new features.In my free time, I enjoy learning new technologies and working on personal projects. I am also an avid reader and enjoy spending time with my family and friends. In my free time, I enjoy learning new technologies and working on personal projects. I am also an avid reader and enjoy spending time with my family and friends.",
+  heroImage: "/images/hero.png",
+};
+
+export const About = {
+  hTag: "About Me",
+  h1Tag: "Hi, I am Nathnael Teshome",
+  pTag2:
+    "I am a software engineer with a passion for building web applications. I have experience in JavaScript, React, and Node.js.I am currently working at a startup where I am responsible for building and maintaining the company's web applications. I am also involved in the design and development of new features.In my free time, I enjoy learning new technologies and working on personal projects. I am also an avid reader and enjoy spending time with my family and friends. In my free time, I enjoy learning new technologies and working on personal projects. I am also an avid reader and enjoy spending time with my family and friends.",
+};
+
+export const Skills = [
+  {
+    id: 1,
+    name: "JavaScript",
+    icon: "/images/skills/javascript.svg",
+  },
+  {
+    id: 2,
+    name: "React",
+    icon: "/images/skills/react.svg",
+  },
+  {
+    id: 3,
+    name: "Node.js",
+    icon: "/images/skills/nodejs.svg",
+  },
+  {
+    id: 4,
+    name: "Next.js",
+    icon: "/images/skills/nextjs.svg",
+  },
+  {
+    id: 5,
+    name: "Tailwind CSS",
+    icon: "/images/skills/tailwindcss.svg",
+  },
+  {
+    id: 6,
+    name: "TypeScript",
+    icon: "/images/skills/typescript.svg",
+  },
+];
+
+export const Experience = [
+  {
+    id: 1,
+    title: "Software Engineer",
+    company: "Tech Company",
+    date: "Jan 2022 - Present",
+    description:
+      "I am a software engineer with a passion for building web applications. I have experience in JavaScript, React, and Node.js.I am currently working at a startup where I am responsible for building and maintaining the company's web applications. I am also involved in the design and development of new features.",
+  },
+  {
+    id: 2,
+    title: "Intern Software Engineer",
+    company: "Tech Company",
+    date: "Jan 2021 - Dec 2021",
+    description:
+      "I am a software engineer with a passion for building web applications. I have experience in JavaScript, React, and Node.js.I am currently working at a startup where I am responsible for building and maintaining the company's web applications. I am also involved in the design and development of new features.",
+  },
+];
+
+export const Projects = [
+  {
+    name: "RateEat",
+    tag: "Featured Project",
+    description:
+      "RateEat is a web application that allows users to rate and review restaurants. It is built with React, Node.js, and MongoDB. The application has a user-friendly interface and allows users to search for restaurants by location, cuisine, and rating. Users can also create an account to save their favorite restaurants and reviews.",
+    imageUrl: "/images/projects/rateeat.png",
+    technologies: ["python", "flask", "mongodb"],
+    github: "http://github.com/rateeat",
+    url: "http://rateeat.com",
+  },
+  {
+    name: "RideShare",
+    tag: "Featured Project",
+    description:
+      "RideShare is a web application that allows users to find and book rides with other users. It is built with React, Node.js, and MongoDB. The application has a user-friendly interface and allows users to search for rides by location, date, and time. Users can also create an account to save their favorite rides and drivers.",
+    imageUrl: "/images/projects/rideshare.png",
+    github: "http://github.com/rideshare",
+    technologies: ["python", "flask", "mongodb"],
+    url: "http://rideshare.com",
+  },
+];
+
+export const Contact = {
+  facebook: "https://www.facebook.com/nathnael.teshome",
+  github: "https:/www.github.com/nathnaelteshome",
+  instagram: "https://www.instagram.com/nathnaelteshome",
+  linkedin: "https://www.linkedin.com/in/nathnaelteshome",
+  twitter: "https://twitter.com/nathnaelteshome",
+  gmail:
+    "https://mail.google.com/mail/?view=cm&fs=1&to=nathnaelteshome10@gmail.com",
+};
