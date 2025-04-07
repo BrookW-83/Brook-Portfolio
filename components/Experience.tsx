d4679f03a255b63992083736cec4a5c3e4c58f82
nathnaelteshome10 <nathnaelteshome10@gmail.com>
2025-04-07 21:38:04 +0300

    feat: update Experience and Project sections with styling improvements and new NavBar component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,7 +5,6 @@ import { useEffect, useRef, useState } from "react";
 const Experience = () => {
   const [activeId, setActiveId] = useState(1);
   const activeExperience = ExperienceData.find((item) => item.id === activeId);
-  const [lineHeight, setLineHeight] = useState(0);
 
   return (
     <section className="py-12 px-6">
@@ -17,17 +16,16 @@ const Experience = () => {
         <div className="flex flex-col md:flex-row">
           {/* Left side - company list */}
           <div className="relative w-full md:w-1/4  mb-8 md:mb-0">
-            <div className="absolute hidden md:block" />
-            <div className="flex md:flex-col overflow-x-auto no-scrollbar">
+            <div className="flex md:flex-col overflow-x-auto md:overflow-x-clip scrollable">
               {ExperienceData.map((exp) => (
                 <button
                   key={exp.id}
                   onClick={() => setActiveId(exp.id)}
-                  className={`flex-shrink-0 md:block md:w-full text-left px-4 py-3 border-b-4 md:border-b-0 transition-all duration-300
+                  className={`flex-shrink-0 md:block md:w-full text-left px-4 py-3 border-b-4 md:border-b-0 
                     ${
                       activeId === exp.id
-                        ? "border-l-4 border-tertiary text-tertiary"
-                        : "border-gray-600 border-l-[1px] hover:text-gray-500"
+                        ? "border-b-4 md:border-l-4 border-tertiary text-tertiary"
+                        : "border-gray-600 md:border-b-0 border-b-4 md:border-l-[1px] hover:text-gray-500"
                     }`}
                 >
                   {exp.company}
--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -6,9 +6,9 @@ import { motion } from "framer-motion";
 
 export default function ProjectSection() {
   return (
-    <section id="projects" className="bg-[#0a192f] py-20 text-white">
+    <section id="projects" className=" py-20 text-white">
       <div className="max-w-6xl mx-auto px-4">
-        <h2 className="text-xl text-teal-400 font-mono mb-8">
+        <h2 className="text-xl text-tertiary font-mono mb-8">
           {ProjectSectionData.sectionId}{" "}
           <span className="text-2xl text-white font-bold ml-2">
             {ProjectSectionData.hTag}
@@ -23,44 +23,50 @@ export default function ProjectSection() {
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.6 }}
               viewport={{ once: true }}
-              className={`relative flex flex-col lg:flex-row ${
+              className={`relative flex flex-col  lg:flex-row ${
                 index % 2 !== 0 ? "lg:flex-row-reverse" : ""
-              } gap-8`}
+              } gap-y-8`}
             >
-              {/* left section */}
-              <div className="lg:w-1/2 relative z-0">
+              {/* image section */}
+              <div className="overflow-hidden min-h-92 lg:w-1/2 relative z-0 ">
                 <div className="overflow-hidden rounded-lg shadow-xl">
                   <Image
                     src={project.imageurl}
                     alt={project.name}
-                    width={700}
-                    height={400}
-                    className="object-cover w-full h-full hover:scale-105 transition-transform duration-300"
+                    fill
+                    className="object-cover w-full h-full filter grayscale hover:grayscale-0 hover:scale-105 transition-transform duration-300"
                   />
                 </div>
               </div>
 
-              {/* right section */}
+              {/* word section */}
 
-              <div className="lg:w-1/2 flex flex-col justify-center lg:items-end text-left lg:text-right lg:-ml-32 z-10">
-                <p className="text-teal-400 font-mono text-sm mb-2">
+              <div
+                className={`relative lg:w-1/2 flex flex-col justify-center lg:min-h-64 lg:items-end text-left ${
+                  index % 2 ? "lg:text-left lg:mr-10" : "lg:text-right"
+                } z-10`}
+              >
+                <p className="hidden lg:block text-tertiary font-mono text-sm lg:mb-2">
                   {project.tag}
                 </p>
-                <h3 className="text-2xl font-bold text-white mb-4 capitalize">
+                <h3 className="absolute lg:relative ml-2 lg:ml-0 text-2xl font-bold text-white mb-4 capitalize">
                   {project.name}
                 </h3>
-
-                <div className="bg-[#112240] text-[#ccd6f6] p-5 rounded-md mb-4 text-sm shadow-md max-w-md">
-                  {project.description}
+                <div className="relative lg:w-full lg:h-48 ">
+                  <div
+                    className={`absolute  -mt-32 lg:mt-0 top-1/2 -translate-y-1/2 ${
+                      index % 2 !== 0 ? "lg:left-0" : "right-0"
+                    } bg-gray-900 text-[#ccd6f6]  border border-slate-200/10 p-5 text-sm shadow-md lg:w-[120%]`}
+                  >
+                    {project.description}
+                  </div>
                 </div>
-
                 <ul className="flex flex-wrap justify-end gap-4 text-xs text-[#8892b0] font-mono mb-4">
                   {project.technologies.map((tech) => (
                     <li key={tech}>{tech}</li>
                   ))}
                 </ul>
-
-                <div className="flex justify-end items-center gap-4 text-teal-400">
+                <div className="flex justify-end items-center gap-4 text-tertiary">
                   {project.github && (
                     <a
                       href={project.github}
